616a80f27ff74b185d0cbc2a59a31343a6475e39
XRPL1-21.I Putu Adi Saputra <putuadi208@gmail.com>
2022-08-08 06:40:09 -0700

    website sixwd

---
+++ scriptHalamanUtama.js
@@ -0,0 +1,21 @@
+let ketua = document.querySelector(".gambar1");
+let zahwa = document.querySelector("#zahwa");
+let bagas = document.querySelector("#bagas");
+let winda = document.querySelector("#winda");
+let ditha = document.querySelector("#ditha");
+
+ketua.addEventListener('click', () => {
+    window.open("https://www.instagram.com/adisaputra5944/");
+});
+zahwa.addEventListener('click', () => {
+    window.open("https://www.instagram.com/zahwa_rahman_/");
+});
+bagas.addEventListener('click', () => {
+    window.open("https://www.instagram.com/bagas.sastrawan/");
+});
+winda.addEventListener('click', () => {
+    window.open("https://www.instagram.com/_wipra/");
+});
+ditha.addEventListener('click', () => {
+    window.open("https://www.instagram.com/dithamaheswariii/");
+});
